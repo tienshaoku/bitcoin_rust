49429239af0da5080a4719e7f5bf02e75dd36542
tienshaoku <tienshaoku@gmail.com>
2026-02-05 08:32:08 +0000

    feat: miner main

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,3 +1,31 @@
+use lib::types::Block;
+use lib::util::Saveable;
+use std::env;
+use std::process::exit;
+
+// use lib/src/bin/block_gen to generate block_file
+
 fn main() {
-    println!("Hello, world!");
+    let (path, steps) = if let (Some(arg), Some(arg2)) = (env::args().nth(1), env::args().nth(2)) {
+        (arg, arg2)
+    } else {
+        eprintln!("Usage: miner <block_file> <steps>");
+        exit(1);
+    };
+    let steps: usize = if let Ok(s @ 1..=usize::MAX) = steps.parse() {
+        s
+    } else {
+        eprintln!("<steps> should be a positive integer");
+        exit(1);
+    };
+
+    let og_block = Block::load_from_file(path).expect("Failed to load block");
+    let mut block = og_block.clone();
+    while !block.header.mine(steps) {
+        println!("mining...");
+    }
+    println!("original: {:#?}", og_block);
+    println!("hash: {}", og_block.header.hash());
+    println!("final: {:#?}", block);
+    println!("hash: {}", block.header.hash());
 }
